6c10bb09bd300cfdd2af2ed77b77e49d127da99a
wenbin <wenbin_mj@163.com>
2026-09-24 10:25:30 +0800

    feat(android): rebrand Android app to ArasDesk and enable Android APK packaging

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -47,7 +47,7 @@ import java.nio.ByteBuffer
 import kotlin.math.max
 import kotlin.math.min
 
-const val DEFAULT_NOTIFY_TITLE = "RustDesk"
+const val DEFAULT_NOTIFY_TITLE = "ArasDesk"
 const val DEFAULT_NOTIFY_TEXT = "Service is running"
 const val DEFAULT_NOTIFY_ID = 1
 const val NOTIFY_ID_OFFSET = 100
@@ -816,13 +816,13 @@ class MainService : Service() {
     private fun initNotification() {
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationChannel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = "RustDesk"
-            val channelName = "RustDesk Service"
+            val channelId = "ArasDesk"
+            val channelName = "ArasDesk Service"
             val channel = NotificationChannel(
                 channelId,
                 channelName, NotificationManager.IMPORTANCE_HIGH
             ).apply {
-                description = "RustDesk Service Channel"
+                description = "ArasDesk Service Channel"
             }
             channel.lightColor = Color.BLUE
             channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
